cf69f75ef2719350ae79f687812ee4193251c4c6
alexar5035 <alexar5035@gmail.com>
2023-02-20 22:13:55 +0000

    first commit

---
+++ robot_config.py
@@ -0,0 +1,69 @@
+# Test the robot
+import math
+import robot_model
+# A: The two-link planar manipulator
+def two_link_manipulator():
+ 	# test values:
+ 	# joint 1: theta = pi/2, a = 1, d = 0, alpha = 0
+ 	# joint 2: theta = pi/2, a = 1, d = 0, alpha = 0
+ 	# 2D array for parameters of each joint
+ 	parameters = [(math.pi/2), 1, 0, 0], [(math.pi/2), 1, 0, 0]
+ 	# passes parameters to kinematic_chain
+ 	hTrans = robot_model.kinematic_chain(parameters)
+ 	# get x, y, z
+ 	x, y, z = robot_model.get_pos(hTrans)
+ 	# get roll, pitch, yaw
+ 	roll, pitch, yaw = robot_model.get_rot(hTrans)
+ 	# print results:
+ 	print('X: ' +x+ 'meters\n Y: ' +y+ 'meters\n Z: ' +z+ 'meters\n Roll: ' +roll+ 'radians\n Pitch: '
+ 	+pitch+ 'radians\n Yaw: ' +yaw+ 'radians')
+
+# B: 6 DoF UR5e robot
+# case 1
+def UR5e1():
+	# insert parameters for joints 1-6 from the slides
+	DHParameters = [[0, 0, 0.1625, math.pi/2],
+					[0, -0.425, 0, 0],
+					[0, -0.3922, 0, 0],
+					[0, 0, 0.1333, math.pi/2],
+					[0, 0, 0.0997, -math.pi/2], 
+					[0, 0, 0.0996, 0]]
+
+	# pass parameters and get homogeneous trans from kinematic_chain
+	hTrans = robot_model.kinematic_chain(DHParameters)
+	# get x, y, z
+ 	x, y, z = robot_model.get_pos(hTrans)
+ 	# get roll, pitch, yaw
+ 	roll, pitch, yaw = robot_model.get_rot(hTrans)
+ 	#print results:
+ 	print('X: ' +x+ 'meters\n Y: ' +y+ 'meters\n Z: ' +z+ 'meters\n Roll: ' +roll+ 'radians\n Pitch: '
+ 	+pitch+ 'radians\n Yaw: ' +yaw+ 'radians')
+
+# case 2: joint 2 theta = -pi/2
+def UR5e2():
+# insert parameters for joints 1-6 from the slides
+	DHParameters = [[0, 0, 0.1625, math.pi/2],
+					[-math.pi/2, -0.425, 0, 0],
+					[0, -0.3922, 0, 0],
+					[0, 0, 0.1333, math.pi/2],
+					[0, 0, 0.0997, -math.pi/2], 
+					[0, 0, 0.0996, 0]]
+
+	# pass parameters and get homogeneous trans from kinematic_chain
+	hTrans = robot_model.kinematic_chain(DHParameters)
+	# get x, y, z
+ 	x, y, z = robot_model.get_pos(hTrans)
+ 	# get roll, pitch, yaw
+ 	roll, pitch, yaw = robot_model.get_rot(hTrans)
+ 	#print results:
+ 	print('X: ' +x+ 'meters\n Y: ' +y+ 'meters\n Z: ' +z+ 'meters\n Roll: ' +roll+ 'radians\n Pitch: '
+ 	+pitch+ 'radians\n Yaw: ' +yaw+ 'radians')
+ 	
+ # show tests results 	
+ if __name__ == '__main__':
+ 	print("Two-Link Planar Manipulator: ")
+ 	two_link_manipulator()
+ 	print("\nUR5e Case 1: ")
+ 	UR5e1()
+ 	print("\nUR5e Case 2: ")
+ 	UR5e2()
---
+++ robot_model.py
@@ -0,0 +1,48 @@
+# Alexa Rodriguez
+import math
+import numpy as np
+
+def dh_transformation(theta, a, d, alpha):
+	#receives the Denavit-Hartenberg parameters 
+	trans = np.array([[math.cos(theta),  -math.sin(theta)*math.cos(alpha), math.sin(theta)*math.sin(alpha), a*math.cos(theta)],
+				    [math.sin(theta), math.cos(theta)*math.cos(alpha), -math.cos(theta)*math.sin(alpha), a*math.sin(theta)],
+				    [0.0, math.sin(alpha), math.cos(alpha), d],
+					[0.0, 0.0, 0.0, 1.0]])
+	# returns a combined homogenous transformation according to this convention. 		
+	return trans
+
+def kinematic_chain(twoDArray):
+	# receives a 2D list/array containing the DH parameters of a robotic manipulator
+	hTrans = np.array([[1.0,  0.0, 0.0, 0.0],
+				    [0.0, 1.0, 0.0, 0.0],
+				    [0.0, 0.0, 1.0, 0.0],
+					[0.0, 0.0, 0.0, 1.0]]) 
+	#set up a loop that scans the rows (joint parameters) and multiplies the transformation to get a total transformation.				
+	for joint in twoDArray:
+		# passes the DH parameters to the dh_transformation function
+		DHresult = dh_transformation(joint[0], joint[1], joint[2], joint[3])
+		# multiply matrix by total
+		hTrans = np.matmul(hTrans, DHresult)
+	# returns a homogenous transformation for the kinematic chain
+	return hTrans
+	
+
+def get_pos(trans): 
+# receives a homogeneous transformation as input and returns the x, y, z components of the position.
+	x = trans[0][3]
+	y = trans[1][3]
+	z = trans[2][3]
+	return x, y, z
+
+
+def get_rot(trans): 
+	# receives a homogeneous transformation as input and returns roll-pith-yaw angles
+	# find roll by using the given formula
+	roll = (math.atan2((trans[2][1]),(trans[2][2])))
+	# find pitch by using the given formula
+	pitch = (math.atan2(-(trans[2][0]), math.sqrt((trans[2][1])**2 + (trans[2][2] **2)))
+	# find yaw by using the given formula
+	yaw = (math.atan2((trans[1][0]), (trans[0][0])))
+	# return the values
+	return roll, pitch, yaw
+
